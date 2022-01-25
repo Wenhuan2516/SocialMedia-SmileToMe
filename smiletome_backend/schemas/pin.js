ac9be152b53f2c6660bff702b6e6eb765943adc7
Wenhuan Tan <wenhuan.tan@bellevuecollege.edu>
2022-01-25 14:23:44 -0800

    backend is done

---
+++ smiletome_backend/schemas/pin.js
@@ -0,0 +1,60 @@
+export default {
+    name: 'pin',
+    title: 'Pin',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'about',
+            title: 'About',
+            type: 'string',
+        },
+        {
+            name: 'destination',
+            title: 'Destination',
+            type: 'url',
+        },
+        {
+            name: 'category',
+            title: 'Category',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                //for more information, please check sanity.io/image-type
+                //hotspot: hotspot makes it possible to reponsively the 
+                //images to different ratios at display time. The default value for hotspot is false
+                hotspot: true
+            }
+        },
+        {
+            name: 'userId',
+            title: 'UserID',
+            type: 'string',
+        },
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy',
+        },
+        {
+            name: 'save',
+            title: 'Save',
+            type: 'array',
+            of: [{ type: 'save'}]
+        },
+        {
+            name: 'comment',
+            title: 'Comments',
+            type: 'array',
+            of: [{ type: 'comment'}]
+        }
+    ]
+}
